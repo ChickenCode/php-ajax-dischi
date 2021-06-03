fa3664f77681c68f8905e7d8013cd30a68157201
Tommaso Cavaletti <cavalettitommaso@gmail.com>
2021-06-03 08:03:53 -0400

    styled

--- index.php
+++ index.php
@@ -73,18 +73,20 @@
         ]
         ];
 
-    function printAlbum($discs) {
-        foreach ($discs as $album) {
+    function printAlbum($disco) {
+        foreach ($disco as $album) {
             $poster = $album["poster"];
             $title = $album["title"];
             $author = $album["author"];
             $genre = $album["genre"];
             $year = $album["year"];
+            echo "<div class='flex flex-column album'>";
             echo "<img src='$poster'>";
             echo "<h2>$title</h2>";
             echo "<p>$author</p>";
             echo "<p>$genre</p>";
             echo "<p>$year</p>";
+            echo "</div>";
         }
       }
 ?>
@@ -96,10 +98,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php
-        printAlbum($discs)
-    ?>
+        <div class="container flex">
+            
+                <?php
+                    printAlbum($discs)
+                ?>
+            
+        </div>
+    
 </body>
 </html>
